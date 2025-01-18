157932feacd3f411fe6419b283ec07bf2e5b3de7
Sukhman2003 <112985333+Sukhman2003@users.noreply.github.com>
2025-01-18 19:35:34 +0530

    commiting some changes

---
+++ firstreact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+import  {ButtonChai} from "./chai"
+
+function App(){
+    return(
+    <>
+     <Chai />
+     <h2>to order chai click button</h2>
+     <ButtonChai />
+    </>
+    )
+}
+
+export default App
---
+++ firstreact/src/chai.jsx
@@ -0,0 +1,18 @@
+function Chai(){
+    return(
+        <>
+        <h1> hello chai</h1>
+        </>
+    )
+}
+
+function ButtonChai(){
+    return(
+        <>
+        <button>click here</button>
+        </>
+    )
+}
+
+export default Chai ;
+export {ButtonChai};
