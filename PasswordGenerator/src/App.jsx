3607cf6d17579d0d7866f0c67cc9573ca4969fef
Sukhman2003 <112985333+Sukhman2003@users.noreply.github.com>
2025-01-28 07:57:47 +0530

    Password Generator

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setlength] = useState(8);
+  const [number, setnumber] = useState(false);
+  const [character, setcharacter] = useState(false);
+  const [passfeild, setpassfeild] = useState("");
+  const copyref=useRef(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const passwordgenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let num = "0123456789";
+    let char = "!@#$%^&*()_+";
+    if (number) {
+      str += num;
+    }
+    if (character) {
+      str += char;
+    }
+    for (let i = 1; i <= length; i++) {
+      pass += str.charAt(Math.floor(Math.random() * str.length + 1));
+    }
+    setpassfeild(pass);
+  }, [length, number, character, setpassfeild]);
+
+  const copypass=()=>{
+    copyref.current.select();
+    window.navigator.clipboard.writeText(passfeild);
 }
 
-export default App
+  useEffect(() => {
+    passwordgenerator();
+  }, [length, number, character, passwordgenerator]);
+  return (
+    <>
+      <div className="text-center w-full max-w-lg mx-auto rounded-3xl my-40 px-4 py-8 text-white bg-gray-500 my-3">
+        Password Generator
+        <div className="flex rounded-lg overflow-hidden mb-8">
+          <input
+            type="text"
+            value={passfeild}
+            placeholder="passfeild"
+            readOnly
+            ref={copyref}
+            className="outline-none w-full my-2 rounded-lg px-3 py-2 bg-white text-gray-900" 
+          />
+          <button 
+          onClick={copypass}
+          className="outline-none my-2 rounded-lg px-3 py-2 bg-blue-700 text-white ">
+            COPY
+          </button>
+        </div>
+        <div className="flex  text—sm gap-x-2 ">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={8}
+              max={25}
+              value={length}
+              onChange={(e) => setlength(e.target.value)}
+            />
+            <label>Length :{length}</label>
+          </div>
+          <div className="flex text—sm gap-x-2">
+            <input
+              type="checkbox"
+              defaultChecked={number}
+              onChange={(e) => setnumber(e.target.checked)}
+            />
+            <label>Number</label>
+          </div>
+          <div className="flex text—sm gap-x-2">
+            <input
+              type="checkbox"
+              defaultChecked={character}
+              onChange={(e) => setcharacter(e.target.checked)}
+            />
+            <label>setcharacter</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
